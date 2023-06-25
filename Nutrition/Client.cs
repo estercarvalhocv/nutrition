f4267c2babf80647f9626c5d68a196d6f96f7476
Ester Borges de Carvalho <esterborgesdecarvalho@gmail.com>
2023-06-24 21:31:59 -0300

    Criando a classe Client

---
+++ Nutrition/Client.cs
@@ -0,0 +1,19 @@
+﻿namespace Nutrition
+{
+    public class Client
+    {
+        public Client(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+
+        public string Name { get; } = string.Empty;
+        public int Age { get; }
+        float Weight { get; set; }
+        float Height { get; set; }
+        float IMC { get; set; }
+        string Contact { get; set; } = String.Empty;
+    }
+}
--- Nutrition/Program.cs
+++ Nutrition/Program.cs
@@ -1,28 +1,27 @@
 ﻿namespace Nutrition
 {
-    public static class Program 
+    public static class Program
     {
-	public static void Main(string[] args)
-	{
-	    int option;
-	    
-	    Console.WriteLine("----- Nutrition Client -----");
-	    Console.WriteLine("1 - Add a new Client");
-	    Console.WriteLine("2 - Edit a Client");
-	    Console.WriteLine("3 - Remove a Cliente");
-	    Console.WriteLine("Choose a option: ");
-	    option = Console.ReadLine();
+        public static void Main(string[] args)
+        {
+            int option;
 
-	    switch(option) {
-		case 1:
-		    break;
-		case 2:
-		    break;
-		case 3:
-		    break;
-		case default:
-		    break;
-	    }
-	}
+            Console.WriteLine("----- Nutrition Client -----");
+            Console.WriteLine("1 - Add a new Client");
+            Console.WriteLine("2 - Edit a Client");
+            Console.WriteLine("3 - Remove a Cliente");
+            Console.WriteLine("Choose a option: ");
+            option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
     }
 }
